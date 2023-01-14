16f8fecf946414d050b9bdd9340e9b7413f76040
Tasin Mir <tasinmir20@gmail.com>
2023-01-14 20:17:56 +0600

    setup

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+
+const app = express();
+
+dotenv.config(); // called dotenv
+
+// Middleware Array
+const middleware = [morgan("dev"), express.static("public"), express.urlencoded({ extended: true }), express.json()];
+console.clear();
+app.use(middleware);
+
+/* Configuration */
+const config = {
+	PORT: process.env.PORT || 1000,
+};
+const nagad = require("./nagad");
+app.get("/*", async (req, res, next) => {
+	try {
+		const nagadURL = await nagad.createPayment({
+			// -> get intellisense here
+			amount: "100",
+			ip: "10.10.0.10",
+			orderId: `${Date.now()}`,
+			productDetails: { a: "1", b: "2" },
+			clientType: "PC_WEB",
+		});
+		//redirect user to the nagad url
+
+		return res.json({ nagadURL });
+	} catch (err) {
+		console.log(err);
+	}
+});
+
+app.listen(config.PORT, () => {
+	console.log(`Server is Running on ${config.PORT}`);
+});
---
+++ nagad.js
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const NagadGateway = require("nagad-payment-gateway");
+
+const config = {
+	apiVersion: "v-0.2.0",
+	baseURL: process.env.BASE_URL,
+	callbackURL: process.env.CALLBACK_URL,
+	merchantID: process.env.MERCHANT_ID,
+	merchantNumber: process.env.MERCHANT_NUMBER,
+	privKey: process.env.privateKey,
+	pubKey: process.env.publicKey,
+	isPath: true,
+};
+const nagad = new NagadGateway(config);
+
+module.exports = nagad;
